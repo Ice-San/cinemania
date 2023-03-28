f4c54b8fae7a6a20dfeafaa1a7e5a7a81ed09db1
Ice-San <rotommine@gmail.com>
2023-03-28 23:24:01 +0100

    CINEMANIA-12 | Homepage | Footer de alta fidelidade | Fix the Footer height!

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const startNowTitle = document.getElementById("start-now-title");
 
 //      Code
 signin.addEventListener("mouseover", () => {
+
+    // Check if contains the reverse animation, if the condution is true it removes them
+    if(signin.classList.contains("start-now-scale-effect-reverse") == true) {
+        signin.classList.remove("start-now-scale-effect-reverse");
+        startNowTitle.classList.remove("start-now-scale-effect-reverse");
+    };
+
     signin.classList.add("start-now-scale-effect");
     startNowTitle.classList.add("start-now-scale-effect");
 });
@@ -17,4 +24,8 @@ signin.addEventListener("mouseover", () => {
 signin.addEventListener("mouseout", () => {
     signin.classList.remove("start-now-scale-effect");
     startNowTitle.classList.remove("start-now-scale-effect");
+
+    // Adding the reverse animation
+    signin.classList.add("start-now-scale-effect-reverse");
+    startNowTitle.classList.add("start-now-scale-effect-reverse");
 });
